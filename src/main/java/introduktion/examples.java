5082694f907d0f51a9201db68fe656267d473814
Georgi Ivanov <114055119+georgiValentinovIvanov@users.noreply.github.com>
2023-08-07 14:52:50 +0300

    add first test

---
+++ src/main/java/introduktion/examples.java
@@ -0,0 +1,13 @@
+package introduktion;
+
+import java.util.Scanner;
+
+public class examples {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username:");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
+    }
+}
